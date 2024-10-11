0bda40f59192f6e65de7d297383e5fa2d091b759
student <student@mail.ru>
2024-10-11 20:12:43 +0300

    ok

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, url_for, redirect
-
+from flask import Flask, abort, render_template, request, url_for, redirect
+import psycopg2
 app = Flask(__name__)
 
 class User:
@@ -19,14 +19,47 @@ users = []
 def create_user():
     login = request.form['login']
     password = request.form['password']
-    users.append(
-        User(login, password)
-    )
+    connection = psycopg2.connect(database="users_application", user="administrator",
+                                  password="root", host="localhost", port="5432")
+    cursor = connection.cursor()
+
+    cursor.execute('''INSERT INTO USERS(login, password)
+    VALUES (%s, %s);''', (login, password))
+
+    connection.commit()
+    connection.close()
+    cursor.close()
+
     return redirect(url_for('index'))
 
+
+@app.route('/user/all')
 @app.route('/user/<int:user_id>')
-def get_user(user_id):
-    return users[user_id].__dict__
+def get_user(user_id=None):
+    connection = psycopg2.connect(database="users_application", user="administrator",
+                                  password="root", host="localhost", port="5432")
+    cursor = connection.cursor()
+
+    if user_id is None:
+        cursor.execute('''SELECT * FROM USERS''')
+        users_data = cursor.fetchall()
+        connection.close()
+        cursor.close()
+
+
+        return [User(i[0], i[1]).__dict__ for i in users_data]
+
+    cursor.execute('''SELECT * FROM USERS WHERE user_order=%s''', [user_id])
+
+    user_data = cursor.fetchall()
+
+    connection.close()
+    cursor.close()
+
+    if user_data.__len__() == 0:
+        return abort(404, f"User with id {user_id} not found")
+
+    return User(user_data[0][0], user_data[0][1]).__dict__
 
 @app.route('/user')
 def get_users():
